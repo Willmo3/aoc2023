03099ebb84b9574176d533c7fc970374de09dec1
Willmo3 <thelordofbouge@gmail.com>
2023-12-05 12:12:52 -0500

    Update number finder to use matrix

--- day3/symbol/src/main.rs
+++ day3/symbol/src/main.rs
@@ -3,39 +3,41 @@ use std::{env, fs};
 fn main() {
     let filename = env::args().skip(1).next().unwrap();
     let contents = fs::read_to_string(filename).unwrap();
+    let contents: Vec<&str> = contents.split("\n").collect();
+
     println!("{:?}", find_numbers(&contents));
 }
 
 // Find where all the numbers are.
-fn find_numbers(s: &str) -> Vec<Number> {
+fn find_numbers(s: &Vec<&str>) -> Vec<Coords> {
     let mut i: usize = 0;
-    let mut start_index: Option<usize> = None;
-    let mut end_index: usize;
-    let mut retval : Vec<Number> = Vec::new();
+    let mut start_index: Option<(usize, usize)> = None;
+    let mut end_index: (usize, usize);
+    let mut retval : Vec<Coords> = Vec::new();
 
-    // Inclusive start index
-    // Exclusive end index.
-    for char in s.chars() {
-        match start_index {
-            None => {
-                if char.is_digit(10) {
-                    start_index = Some(i);
-                };
-            },
-            Some(start) => {
-                if !char.is_digit(10) {
-                    end_index = i;
-                    retval.push(Number { start_index: start, end_index } );
-                    start_index = None;
-                };
+    for row in s.iter() {
+        let mut j: usize = 0;
+        for char in row.chars() {
+            match start_index {
+                None => {
+                    if char.is_digit(10) {
+                        start_index = Some((i, j));
+                    };
+                },
+                Some(start) => {
+                    if !char.is_digit(10) {
+                        end_index = (i, j);
+                        retval.push(Coords { start_index: start, end_index });
+                        start_index = None;
+                    };
+                }
             }
+            j += 1;
         }
-
         i += 1;
     }
 
     retval
-
 }
 
 // loop through each character
@@ -44,9 +46,9 @@ fn find_numbers(s: &str) -> Vec<Number> {
 // add number struct
 
 #[derive(Debug)]
-struct Number {
-    start_index: usize,
-    end_index: usize,
+struct Coords {
+    start_index: (usize, usize),
+    end_index: (usize, usize)
 }
 
 // Given an index collection of data:
